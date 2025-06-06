38474e6289c21c86d0ff5434e2cc5279bd450100
kaci <kacicox212@gmail.com>
2025-06-06 15:48:16 -0500

    access a locker

--- 1-java-languge-fundamentals/exercises/JavaSummative.java
+++ 1-java-languge-fundamentals/exercises/JavaSummative.java
@@ -3,10 +3,11 @@ import java.util.Random;
 
 
 public class JavaSummative {
-	String pin = String.format("%04d", new Random().nextInt(10000));
+	private Scanner random;
+	String pin = String.format("%04d", random.nextInt(10000));
 	private static int[] lockerNumbers = {1, 2, 3};
 	private static String[] lockerPins = new String[3];
-	private static Scanner scnaner = new Scanner(System.in);
+	private static Scanner scanner = new Scanner(System.in);
 	private static int count = 0;
 
 
@@ -33,9 +34,9 @@ public class JavaSummative {
 				System.out.println("you chose to rent a locker");
 				rentLocker(args);
 				break;
-
-				case 2:
+			case 2:
 				System.out.println("you chose to access to a locker");
+				accessLocker(args);
 				break;
 			case 3:
 				System.out.println("you chose to release a locker");
@@ -45,32 +46,63 @@ public class JavaSummative {
 			System.exit(0);
 		}
 	}
-
+//2. [Feature] Rent a Locker
+	//● Assign next available locker number
+	//● Generate and display a 4-digit PIN (as a String with leading zeros)
+	//● Store locker and PIN in array
+	//● Remove option if no lockers available
 
 	public static void rentLocker(String[] args) {
 		System.out.println("assigning next available locker number...");
-		//assign locker number
-		//check that locker is available else return to main menu
+
 		for (int i = 0; i < lockerPins.length; i++) {
 			if (lockerPins[i] == null) {
-				//assign pin with random
-				lockerPins[i] = String.format("%04d", new Random().nextInt(1000));
-				// assign locker number by adding to i
+				lockerPins[i] = String.format("%04d", new Random().nextInt(10000));
 				System.out.println("assigned locker " + (i + 1));
 				System.out.println("your pin is " + lockerPins[i]);
-				count++;
+				lockerNumbers[count] = i + 1;
 				main(args);
+
 				{
-					//if not enough locker numbers then return to main menu
 					if (count == 3) {
 						System.out.println("no lockers available, returning to main menu");
 						main(args);
+						break;
 					}
 				}
 			}
 		}
 	}
+//	3. [Feature] Access a Locker
+	//● Prompt user for locker number and PIN
+	//● Validate input against stored data
+	//● Grant access if match is found, else show error
+
+	public static void accessLocker(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("please enter your locker number");
+		int accessLocker = Integer.parseInt(scanner.nextLine());
+		System.out.println("enter your pin");
+		int accessPin = Integer.parseInt(scanner.nextLine());
+
+
+		for (int i = 0; i < lockerPins.length; i++) {
+			if (lockerPins[i].equals(String.valueOf(accessPin)) && lockerNumbers[i] == accessLocker) {
+				System.out.println("access granted. returning to main menu");
+				//System.out.println( lockerPins[i] + accessPin);
+				main(args);
+			}
+			else {
+				System.out.println("access denied. returning to main menu");
+				//System.out.println(lockerPins[i] + accessPin);
+				main(args);
+				break;
+			}
+		}
+	}
 }
+
+
 
 
 
