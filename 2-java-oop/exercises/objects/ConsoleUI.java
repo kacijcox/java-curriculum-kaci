45323ee6fb83f0c35471e0b51767411bde4eb398
kaci <kacicox212@gmail.com>
2025-06-10 08:44:43 -0500

    4

---
+++ 2-java-oop/exercises/objects/ConsoleUI.java
@@ -0,0 +1,48 @@
+package objects;
+
+import java.util.Scanner;
+
+public class ConsoleUI {
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static String getString(String prompt) {
+		System.out.print(prompt);
+		return scanner.nextLine();
+	}
+
+	public static int getInt(String prompt) {
+		while (true) {
+			try {
+				System.out.print(prompt);
+				return Integer.parseInt(scanner.nextLine());
+			} catch (NumberFormatException e) {
+				System.out.println("enter a valid number");
+			}
+		}
+	}
+
+	public static double getDouble(String prompt) {
+		while (true) {
+			try {
+				System.out.print(prompt);
+				return Double.parseDouble(scanner.nextLine());
+			} catch (NumberFormatException e) {
+				System.out.println("enter valid number");
+			}
+		}
+	}
+
+	public static boolean getBoolean(String prompt) {
+		while (true) {
+			System.out.print(prompt + " (y/n): ");
+			String input = scanner.nextLine().toLowerCase();
+			if (input.equals("y") || input.equals("yes")) {
+				return true;
+			} else if (input.equals("n") || input.equals("no")) {
+				return false;
+			} else {
+				System.out.println("enter y/n");
+			}
+		}
+	}
+}
