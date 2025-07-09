1a6298d7adb9e957a687a8016d14cf584a435793
kaci <kacicox212@gmail.com>
2025-07-09 16:30:51 -0500

    issue #25 perishsable product test

--- JavaConsoleApp/test/model/PerishableProductTest.java
+++ JavaConsoleApp/test/model/PerishableProductTest.java
@@ -1,4 +1,21 @@
 package model;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class PerishableProductTest {
+
+	@Test
+	public void PerishableProduct_isPerishable_perishableTrue() {
+		PerishableProduct product = new PerishableProduct(true);
+
+		Assertions.assertEquals(product.isPerishable, true);
+	}
+
+	@Test
+	public void PerishableProduct_isPerishable_perishableFalse() {
+		PerishableProduct product = new PerishableProduct(false);
+
+		Assertions.assertEquals(product.isPerishable, false);
+	}
 }
